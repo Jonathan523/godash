2d966406578f3e9a7b4faa64ff30d0471fc4e6d4
Florian Hoss <mail@florianhoss.de>
2022-10-23 10:45:44 +0200

    Use d3 to manipulate weather

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,17 @@
+import { select } from "https://cdn.skypack.dev/d3-selection@3";
+
 const WsType = { Weather: 0, System: 1 };
 const apiBase = window.location.origin + "/api";
 let socket = new WebSocket(apiBase.replace("http", "ws") + "/system/ws");
-const weatherIcon = document.getElementById("weatherIcon");
-const weatherTemp = document.getElementById("weatherTemp");
-const weatherDescription = document.getElementById("weatherDescription");
-const weatherHumidity = document.getElementById("weatherHumidity");
-const weatherSunrise = document.getElementById("weatherSunrise");
-const weatherSunset = document.getElementById("weatherSunset");
 
 socket.onmessage = (event) => {
   const parsed = JSON.parse(event.data);
   if (parsed.ws_type === WsType.Weather) {
-    weatherIcon.setAttribute("xlink:href", "#" + parsed.message.weather[0].icon);
-    weatherTemp.innerHTML = parsed.message.main.temp + " " + parsed.message.units;
-    weatherDescription.innerHTML = parsed.message.weather[0].description;
-    weatherHumidity.innerHTML = parsed.message.main.humidity + "%";
-    weatherSunrise.innerHTML = parsed.message.sys.str_sunrise;
-    weatherSunset.innerHTML = parsed.message.sys.str_sunset;
+    select("#weatherIcon").attr("xlink:href", "#" + parsed.message.weather[0].icon);
+    select("#weatherTemp").text(parsed.message.main.temp);
+    select("#weatherDescription").text(parsed.message.weather[0].description);
+    select("#weatherHumidity").text(parsed.message.main.humidity);
+    select("#weatherSunrise").text(parsed.message.sys.str_sunrise);
+    select("#weatherSunset").text(parsed.message.sys.str_sunset);
   }
 };
