90b7d3d130b647f672a2825ca4fe5edde125a974
Florian Hoss <mail@florianhoss.de>
2022-10-25 12:03:27 +0200

    Remove hostname

--- system/host.go
+++ system/host.go
@@ -1,14 +1,11 @@
 package system
 
 import (
-	"github.com/shirou/gopsutil/v3/host"
 	"runtime"
 )
 
 func staticHost() Host {
 	var h Host
-	info, _ := host.Info()
 	h.Architecture = runtime.GOARCH
-	h.HostName = info.Hostname
 	return h
 }
