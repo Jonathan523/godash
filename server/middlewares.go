52f16c99667b38838eb1fa217cdbf2e93a0531de
Florian Hoss <mail@florianhoss.de>
2022-11-27 11:59:57 +0100

    Add cache

--- server/middlewares.go
+++ server/middlewares.go
@@ -1,9 +1,16 @@
 package server
 
 import (
+	"context"
+	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/hertz-contrib/gzip"
 )
 
+func CacheMiddleware() app.HandlerFunc {
+	return func(ctx context.Context, c *app.RequestContext) {
+		c.Header("Cache-Control", "public, max-age=604800, immutable")
+	}
+}
 func (server *Server) setupMiddlewares() {
 	server.Router.Use(gzip.Gzip(gzip.DefaultCompression))
 }
--- server/router.go
+++ server/router.go
@@ -10,20 +10,18 @@ func (server *Server) setupRouter() {
 	server.Router.GET("/", server.goDash)
 	server.Router.GET("/ws", webSocket)
 
-	server.serveStatic("static")
-	server.serveStatic("storage/icons")
-
-	server.Router.GET("/robots.txt", func(c context.Context, ctx *app.RequestContext) {
-		ctx.File(TemplatesFolder + "/robots.txt")
-	})
-	server.Router.GET("/favicon.ico", func(c context.Context, ctx *app.RequestContext) {
-		ctx.File("static/favicon/favicon.ico")
-	})
-
 	server.Router.NoMethod(func(c context.Context, ctx *app.RequestContext) {
 		ctx.Redirect(consts.StatusPermanentRedirect, []byte("/"))
 	})
 	server.Router.NoRoute(func(c context.Context, ctx *app.RequestContext) {
 		ctx.Redirect(consts.StatusPermanentRedirect, []byte("/"))
+	})
+
+	server.Router.Use(CacheMiddleware())
+	server.serveStatic("static")
+	server.serveStatic("storage/icons")
+
+	server.Router.GET("/robots.txt", func(c context.Context, ctx *app.RequestContext) {
+		ctx.String(consts.StatusOK, "User-agent: *\nDisallow: /")
 	})
 }
