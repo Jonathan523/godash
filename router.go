5aca62e586f43d39e24fa09ea2c28f9dc2c2399c
Florian Hoss <mail@florianhoss.de>
2022-12-19 13:04:34 +0100

    Also cache static files

--- router.go
+++ router.go
@@ -4,9 +4,14 @@ func (g *goDash) setupRouter() {
 	g.router.GET("/", g.index)
 	g.router.GET("/ws", g.ws)
 	g.router.GET("/robots.txt", robots)
-	g.router.Static("/static", "static")
+
+	static := g.router.Group("/static")
+	static.Use(longCacheLifetime)
+	static.Static("/", "static")
+
 	storage := g.router.Group("/storage")
 	storage.Use(longCacheLifetime)
 	storage.Static("/icons", "storage/icons")
+
 	g.router.RouteNotFound("/*", redirectHome)
 }
