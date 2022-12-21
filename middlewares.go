eea845820688a4e9acdd273947c4d320489fb394
Florian Hoss <mail@florianhoss.de>
2022-12-21 16:07:35 +0100

    Update middlewares.go

--- middlewares.go
+++ middlewares.go
@@ -8,7 +8,7 @@ import (
 
 func longCacheLifetime(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		c.Response().Header().Set(echo.HeaderCacheControl, "public, max-age=31536000, immutable")
+		c.Response().Header().Set(echo.HeaderCacheControl, "public, max-age=31536000")
 		return next(c)
 	}
 }
